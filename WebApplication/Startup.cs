1e0d79b6aafcafa90b968181cfd2f9cf521b9cc0
picarro-mshaynyuk <116036783+picarro-mshaynyuk@users.noreply.github.com>
2024-09-17 15:55:50 -0700

    init commit

---
+++ WebApplication/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Data.Entity;
+using WebApplication.Features.Authorization;
+using WebApplication.Features.SamplePersistentConnection;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication.Startup))]
+namespace WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            ConfigureAuth(app);
+
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration();
+                app.MapSignalR();
+                app.MapSignalR<DemoPersistentConnection>("/demo");
+                app.MapSignalR<AuthorizationPersistentConnection>("/authPersistConnection");
+                map.MapSignalR(hubConfiguration);
+            });
+           /*     
+            app.MapSignalR();
+            app.MapSignalR<DemoPersistentConnection>("/Connections/DemoPersistentConnection");
+            app.MapSignalR<AuthorizationPersistentConnection>("/Connections/AuthorizationPersistentConnection");
+
+            app.Map("/EnableDetailedErrors", map =>
+            {
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableDetailedErrors = true
+                };
+
+                map.MapSignalR(hubConfiguration);
+            });*/
+        }
+    }
+}
